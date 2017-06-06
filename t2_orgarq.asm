656e7285dcc51e149059faad1f2f612b1c226d6e
ReJJCM <rers162@gmail.com>
2017-06-06 18:59:58 -0300

    Dig_1 function implemented

--- t2_orgarq.asm
+++ t2_orgarq.asm
@@ -32,35 +32,48 @@
 main:
 
 li $t0, 0    #contador
-li $t1,1 #aquxiliar
+li $t1,1 #auxiliar
 li $t8,0 #auxiliar
-li $t2, 44 #tamanho de cada linha
+lw $a3, NUM #loop externo (não esquecer de multiplicar por 4 o NUM,$a3)
+li $t2, 0 #tamanho de cada linha
+li $a2, -4
 
   addiu $sp, $sp, -4
   sw    $ra, 0($sp)
   	#colocar seu codigo
-	
+	jal dig_1
 	lw    $ra, 0($sp)
 	addiu $sp, $sp, 4
 	li	  $v0, 10
 	syscall
   jr    $ra		 
 
-dig_1: # FIX
+dig_1:
+
+
   addiu $sp, $sp, -4 # Atualizar o tamanho da pilha de acordo com os seus registradores!
 	sw    $ra, 0($sp)
-	# Colocar aqui o seu codigo!
-	
-	li $t9,10
-  	li $a1,11
+		
+	li $t9,10 #para o caulculo do somador
+  	li $a1,11 #para o cálculo do resultado
 	li $t3, 0 #somador
+	mul $s0, $a3, 4 #define o tamanho do loop externo
+loopExterno:
+	addi $t2,$t2, 44 #linha interna
+	addi $a2, $a2, 4 #j++
+	beq $a2,$s0, resultado
+
 loop: 
-	lw $t4, CPF($t0)
-	sub $t5, $t9, $t0
-	add $t3, $t3, $t5
+	lw $t4, CPF($t0) #carrega o valor do CPF no registrador
+	sub $t5, $t9, $t0 #10-i  #t5 é uma variavel auxiliar de results
+	mul $s1, $t4, $t5 #CPF[pos][i]*10-i
+	add $t3, $t3, $s1 #somador total
 	
-	addi $t0, $t0, 4
-	bne $t0, $t2, loop
+	addi $t0, $t0, 4 #i++
+	bne $t0, $t2, loop #se não é o fim do loop continua
+	beq $t0,$t2, loopExterno #se chegar no fim do i vai pro loop externo
+	
+resultado:
 	
 	rem $t5, $t3, 11 #resultado
 
@@ -68,21 +81,16 @@ verificacaoResultado:
 beqz $t5, retornaZero
 beq $t5, $t1, retornaZero
 
-	sub $t6, $a1, $t5 #return 11-resultado;
-	li $v0,1
-	move  $a0, $t6
-	syscall
+sub $v0, $a1, $t5 #return 11-resultado;
 
 retornaZero:
+li $v0, 0
 
-li $v0,1
-move  $a0, $t8
-syscall
+#v0 TEM O RESULTADO!!!!!!
 
-
-	lw    $ra, 0($sp)
-	addiu $sp, $sp, 4 # Atualizar o tamanho da pilha de acordo com os seus registradores!
-  jr    $ra
+lw    $ra, 0($sp)
+addiu $sp, $sp, 4 # Atualizar o tamanho da pilha de acordo com os seus registradores!
+ jr    $ra
 
 dig_2:
   addiu $sp, $sp, -4 # Atualizar o tamanho da pilha de acordo com os seus registradores!
